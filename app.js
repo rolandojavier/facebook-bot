f01ab95310ba879023c91bf6e2a98c122c2e2ae9
Rolando Ramos <rolando@goread.com>
2018-05-07 22:00:52 -0500

    Added Helpers

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const verificationController = require('./controllers/verification')
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen((process.env.PORT || 3000), () => console.log('Webhook server is listening...'));
 
+const verificationController = require('./controllers/verification');
+const messageWebhookController = require('./controllers/messageWebhook');
+
 app.get('/', verificationController);
+app.post('/', messageWebhookController);
