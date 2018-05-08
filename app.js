92a5c0882c139a763f5b08111e488e68c42221b7
Rolando Ramos <rolando@goread.com>
2018-05-07 20:55:59 -0500

    Added Controllers

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var app = express()
-app.use(bodyParser.urlencoded({extended: false}));
+const app = express();
+
 app.use(bodyParser.json())
-app.listen((process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// Server index page
-app.get("/", function (req, res) {
-    res.send("Deployed!");
-});
-
-// Facebook Webhook
-// Used for verification
-app.get("/webhook", function (req, res) {
-    if (req.query["hub.verify_token"] == process.env.VERIFICATION_TOKEN) {
-        console.log("Verified webhook");
-        res.status(200).send(req.query["hub.challenge"]);
-    } else {
-        console.error("Verification failed. The tokens do not match.");
-        res.sendStatus(403);
-    }
-});
+app.listen((process.env.PORT || 3000), () => console.log('Webhook server is listening...'));
